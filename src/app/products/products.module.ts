f42c6031482e2b719cbfbe0c4ec2bb4d01e73921
Edgar Hernando Diaz <edgarh2dm3@gmail.com>
2024-07-17 20:22:32 -0500

    order

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,26 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ProductsRoutingModule } from './products-routing.module';
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
-import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
-import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
-import { PrimeNgModule } from '../prime-ng/prime-ng.module';
-import { OrderComponent } from './pages/order/order.component';
-import { ToggleCasePipe } from './pipes/toggle-case.pipe';
 import { CanFlyPipe } from './pipes/can-fly.pipe';
+import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { OrderComponent } from './pages/order/order.component';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+import { ProductsRoutingModule } from './products-routing.module';
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
+import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 
 
 @NgModule({
   declarations: [
     BasicsPageComponent,
     NumbersPageComponent,
-    UncommonPageComponent,
     OrderComponent,
+    UncommonPageComponent,
 
     // Pipes
-    ToggleCasePipe,
     CanFlyPipe,
+    ToggleCasePipe,
   ],
   imports: [
     CommonModule,
